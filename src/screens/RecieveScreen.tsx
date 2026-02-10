9084d82bd0088b37b3bb4a929b231ef1846678ae
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-02-10 20:30:00 +0530

    complte beaturiful ui of recievescreen

--- src/screens/RecieveScreen.tsx
+++ src/screens/RecieveScreen.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 import React, { FC, useState } from 'react'
 import LinearGradient from 'react-native-linear-gradient';
 import { sendStyles } from '../styles/sendStyles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from '../components/global/Icon';
 import CustomText from '../components/global/CustomText';
+import BreakerText from '../components/ui/BreakerText';
+import { Colors } from '../utils/Constants';
+import ORGenrateModal from '../components/modals/QRGenerateModal';
+import DeviceInfo from 'react-native-device-info';
+import LottieView from 'lottie-react-native';
+import { goBack } from '../utils/NavigationUtil';
+import QRGenerateModal from '../components/modals/QRGenerateModal';
+
 
 const RecieveScreen:FC = () => {
 
@@ -40,8 +48,47 @@ const RecieveScreen:FC = () => {
        style={{textAlign : "center"}}>
         Ensure your device is connected to the sender's hotspot network
       </CustomText>
+        <BreakerText text='or'/>
+      
+      <TouchableOpacity
+      style = {sendStyles.qrButton}
+      onPress={()=> setIsScannerVisible(true)} 
+      >
+
+        <Icon name='qrcode'
+         iconFamily='MaterialCommunityIcons'
+         color= {Colors.primary}
+         size={16}/>
+
+         <CustomText fontFamily='Okra-Bold' color={Colors.primary}>
+          Show QR
+         </CustomText>
+      </TouchableOpacity>
+
        </View>
+     
+     <View style = {sendStyles.animationContainer}>
+      <View style = {sendStyles.lottieContainer}>
+      <LottieView  style = {sendStyles.lottie}
+       source={require("../assets/animations/scan2.json")}
+        autoPlay 
+        loop = {true}
+        hardwareAccelerationAndroid/>
+      </View>
+
+      <Image source={require('../assets/images/sword.jpg')}
+      style = {sendStyles.profileImage}/>
+     </View>
+
+     <TouchableOpacity style = {sendStyles.backButton} onPress={goBack}>
+      <Icon name='arrow-back' iconFamily='Ionicons' size={16} color='#000'/>
+     </TouchableOpacity>
+     
     </View>
+    { isScannerVisible && (
+      <QRGenerateModal visible = {isScannerVisible}
+        onClose={()=> setIsScannerVisible(false)}/>
+    )}
    </LinearGradient>
   )
 }
