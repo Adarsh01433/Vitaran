7081ac6184c2200e1540b6fa72a699b5459152ee
Adarsh01433 <adarshtiwarip1433@gmail.com>
2026-02-10 20:00:54 +0530

    complete BreakerText com

--- src/screens/RecieveScreen.tsx
+++ src/screens/RecieveScreen.tsx
@@ -1,11 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { FC, useState } from 'react'
+import LinearGradient from 'react-native-linear-gradient';
+import { sendStyles } from '../styles/sendStyles';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from '../components/global/Icon';
+import CustomText from '../components/global/CustomText';
 
-const RecieveScreen = () => {
-  return (
-    <View>
-      <Text>RecieveScreen</Text>
+const RecieveScreen:FC = () => {
+
+  const [qrValue , setQRValue] = useState('');
+  const [isScannerVisible, setIsScannerVisible] = useState(false)
+
+
+    return (
+   <LinearGradient 
+   colors={['#ffffff', '#4DA0DE', '#3387C5']}
+     style = {sendStyles.container}
+     start={{x : 0 , y : 1}}
+     end={{ x : 0 , y : 0}}>
+      <SafeAreaView/>
+    <View style = {sendStyles.mainContainer}>
+       <View style = {sendStyles.infoContainer}>
+     <Icon name='blur-on'
+      iconFamily='MaterialIcons'
+      color='#fff'
+      size={40}/>
+
+      <CustomText 
+      fontFamily='Okra-Bold'
+      color='#fff'
+      fontSize={16}
+       style={{marginTop : 20}}>
+        Recieving from nearby devices
+      </CustomText>
+
+      <CustomText fontFamily='Okra-Medium'
+      color='#fff'
+       fontSize={12}
+       style={{textAlign : "center"}}>
+        Ensure your device is connected to the sender's hotspot network
+      </CustomText>
+       </View>
     </View>
+   </LinearGradient>
   )
 }
 
